c5278093c6518554efb58bf7fc57e8d7a695ce17
AndreeaGabriela1 <126598376+AndreeaGabriela1@users.noreply.github.com>
2023-10-26 01:03:32 +0300

    Delete Abstract Factory directory

--- Abstract Factory/src/LightCheckbox.java
+++
@@ -1,7 +0,0 @@
-// Light theme checkbox
-public class LightCheckbox implements Checkbox {
-    @Override
-    public void render() {
-        System.out.println("Rendering a Light Theme Checkbox");
-    }
-}
